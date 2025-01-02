d60904399448e556889010ad78b2fba24c49ed9f
LisovichOleksandr <lisovyhs@gmail.com>
2025-01-02 09:42:56 +0200

    Add security Basic Authentification

--- manager-app/src/main/java/com/selm/manager/config/ClientBeans.java
+++ manager-app/src/main/java/com/selm/manager/config/ClientBeans.java
@@ -4,6 +4,7 @@ import com.selm.manager.client.RestClientProductsRestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,9 +12,12 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient productsRestClient(
-            @Value("${selm.cervices.catalogue.uri:http://localhost:8081}") String catalogueBaseUri){
+            @Value("${selm.cervices.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            @Value("${selm.cervices.catalogue.username:}") String catalogueUserName,
+            @Value("${selm.cervices.catalogue.password:}") String cataloguePassword){
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUserName, cataloguePassword))
                 .build());
     }
 }
